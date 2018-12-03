841aaf837a36e9eb701389a9435f234f60114670
Alexander Colen <alexanderc@live.nl>
2018-12-03 13:10:56 +0100

    Day 3 puzzle 2 fixed
    (With the most excellent code ever made by a human being)

--- Code/day3.py
+++ Code/day3.py
@@ -41,11 +41,42 @@ def puzzle1():
 
 
 def puzzle2():
-    file_input = open('../Input/input_day3.txt', 'r')
+    lines = open('../Input/input_day3.txt', 'r').readlines()
 
     grid = fillGrid()
 
-    return 0
+    noOverlapFound = False
+    foundID = 0
+    while not noOverlapFound:
+        for key, value in grid.items():
+            if grid[key] > 0:
+                ID = grid[key]
+                try:
+                    for i, line in enumerate(lines):
+                        if i == ID - 1:
+                            print(f'Checking ID: {ID}')
+
+                            inches_from_left = int(line.split('@')[1].split(',')[0].strip())
+                            inches_from_top = int(line.split(',')[1].split(':')[0])
+                            rect_width = int(line.split(':')[1].split('x')[0].strip())
+                            rect_height = int(line.split('x')[1].strip())
+
+                            # Increment the correct parts of the grid for the line.
+                            for x in range(inches_from_left, inches_from_left + rect_width):
+                                for y in range(inches_from_top, inches_from_top + rect_height):
+                                    print(f'X: {x} - Y: {y} - Value: {grid[(x, y)]}')
+                                    if grid[(x, y)] <= 0:
+                                        raise OverlapException
+                            foundID = ID
+                            noOverlapFound = True
+                except OverlapException:
+                    continue
+
+    return foundID
+
+
+class OverlapException(Exception):
+    pass
 
 
 print(f'Puzzle 1 outcome: {puzzle1()}')
