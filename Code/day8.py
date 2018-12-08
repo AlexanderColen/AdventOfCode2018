8da40719a60106d50ee691276faa81969b27f444
Alexander Colen <alexanderc@live.nl>
2018-12-08 17:39:03 +0100

    Day 8 puzzle 2 finished.
    That was a fucking pain and a half.

--- Code/day8.py
+++ Code/day8.py
@@ -1,3 +1,7 @@
+import string
+import operator
+
+
 def getHeaderFromData(data):
     header = [data[0], data[1]]
     data.remove(data[0])
@@ -16,39 +20,64 @@ def getMetadataFromData(number_of_entries, data):
     return metadata, data
 
 
-def checkNodeChildren(data, number_of_children):
+def checkNodeChildren(data, number_of_children, count):
     nodes = []
+    saved_IDs = []
+    # Save the IDs.
+    for x in range(number_of_children):
+        saved_IDs.append(count)
+        count += 1
+    # Now recursively loop through.
     for x in range(number_of_children):
         header, data = getHeaderFromData(data=data)
         new_nodes = []
         if header[0] > 0:
-            new_nodes, data = checkNodeChildren(data=data, number_of_children=header[0])
+            new_nodes, data = checkNodeChildren(data=data, number_of_children=header[0], count=count)
             nodes += new_nodes
         metadata, data = getMetadataFromData(number_of_entries=header[1], data=data)
-        nodes.append(Node(header=header, metadata=metadata, data=data, children=new_nodes))
+        nodes.append(Node(ID=saved_IDs[x], header=header, metadata=metadata, data=data.copy(), children=new_nodes))
 
     return nodes, data
 
 
-def getAllNodes(data_input):
+def getAllNodes(data_input, count=0):
     data = list(map(int, data_input.split()))
     nodes = []
     header, data = getHeaderFromData(data=data)
-    new_nodes, data = checkNodeChildren(data=data, number_of_children=header[0])
+    base_data = data.copy()[:-header[1]]
+    saved_ID = count
+    count += 1
+    new_nodes, data = checkNodeChildren(data=data.copy(), number_of_children=header[0], count=count)
     nodes += new_nodes
     # Create base node and add it to the list.
     metadata, data = getMetadataFromData(number_of_entries=header[1], data=data)
-    nodes.append(Node(header=header, metadata=metadata, data=data, children=new_nodes))
+    nodes.append(Node(ID=saved_ID, header=header, metadata=metadata, data=base_data, children=new_nodes))
 
     return nodes
+
+
+def calculateNodeValue(node):
+    value = 0
+    # No children means value of combined metadata.
+    if len(node.children) == 0:
+        value += sum(node.metadata)
+    # Otherwise metadata entries are child node indexes.
+    else:
+        node.children.sort(key=operator.attrgetter('ID'))
+        for i in node.metadata:
+            try:
+                value += calculateNodeValue(node.children[:node.header[0]][i - 1])
+            except IndexError:
+                {}
+    return value
 
 
 def puzzle1():
     file_input = open('../Input/input_day8.txt', 'r')
     test_input = '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'
 
-    # nodes = getAllNodes(test_input)
-    nodes = getAllNodes(file_input.readline())
+    nodes = getAllNodes(test_input)
+    # nodes = getAllNodes(file_input.readline())
 
     metadata_sum = 0
     for node in nodes:
@@ -61,31 +90,24 @@ def puzzle2():
     file_input = open('../Input/input_day8.txt', 'r')
     test_input = '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'
 
-    nodes = getAllNodes(test_input)
-    # nodes = getAllNodes(file_input.readline())
+    # nodes = getAllNodes(test_input)
+    nodes = getAllNodes(file_input.readline())
 
-    value = 0
-    for node in nodes:
-        print(node)
-        # No children means value of combined metadata.
-        if len(node.children) == 0:
-            value += sum(node.metadata)
-        # Otherwise metadata entries are child node indexes.
-        else:
-            value += 0
-
-    return value
+    return calculateNodeValue(nodes[len(nodes) - 1])
 
 
 class Node:
-    def __init__(self, header, metadata, data, children):
+    def __init__(self, ID, header, metadata, data, children):
+        self.ID = ID
         self.header = header
         self.metadata = metadata
         self.data = data
         self.children = children
+        self.value = 0
 
     def __str__(self):
-        return f'Header: {self.header} - Metadata: {self.metadata} - Children: {len(self.children)}'
+        return f'ID: {self.ID} - Header: {self.header} - Metadata: {self.metadata} - ' \
+               f'Children: {len(self.children)} - Data: {self.data}'
 
 
 print(f'Puzzle 1 outcome: {puzzle1()}')
